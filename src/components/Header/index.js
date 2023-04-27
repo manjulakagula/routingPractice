64bf7f45223537db2d5f248a01924725f0483543
Your Name <you@example.com>
2023-04-27 08:28:30 +0000

    routingPractice

---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <div className="nav-container">
+    <div className="logo-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        alt="wave"
+        className="logo"
+      />
+      <h1 className="wave-heading">Wave</h1>
+    </div>
+
+    <ul className="nav-menu">
+      <li className="list">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="list">
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li className="list">
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Header
